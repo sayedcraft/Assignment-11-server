bc498a5b9ac1a1f926aabac8f5ec25f9b9c0b4c8
Sayed <muhammadsaye741@gmail.com>
2026-05-23 00:41:18 +0600

    Implement addBook function

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+const uri = "mongodb+srv://books_db:9BVaEPfMZOidy9hu@cluster0.at2amoq.mongodb.net/?appName=Cluster0";
+
 // middlewere
 app.use(express.json());
 app.use(cors());
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+    const db = client.db('booksDB')
+    const booksCollection= db.collection('books')
+
+    app.post('/books',async(req,res)=>{
+      const bookData=req.body
+      const result=await booksCollection.insertOne(bookData)
+      res.send(result)
+    })
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Book server is running");
