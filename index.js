83790ae54f4ad969889d157b7a6ca5e6f02c4c36
Sayed <muhammadsaye741@gmail.com>
2026-05-27 05:58:50 +0600

    Fix some error

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
+    origin: [
+    "http://localhost:5173",              
+    "https://book-courier-74d78.web.app",       
+    "https://book-courier-74d78.firebaseapp.com" 
+  ],
+  credentials: true
   }),
 );
 
